59cc79d8e2da2445fc116fcf1185cd6d84f5fa49
Andres <zadorozhnyi.dev@gmail.com>
2021-09-28 23:47:52 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+      <TodoList />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
